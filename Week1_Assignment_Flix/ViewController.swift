0ae03eba5c8e1d5836df35430e196b3617121295
Alicia Washington <aliciawashington874@gmail.com>
2019-10-15 17:47:54 -0400

    Initial Commit

---
+++ Week1_Assignment_Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week1_Assignment_Flix
+//
+//  Created by Alicia Washington on 10/15/19.
+//  Copyright © 2019 Alicia Washington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
