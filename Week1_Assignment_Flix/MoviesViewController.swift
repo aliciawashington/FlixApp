cfb1f9ede8d41fadb1d1aace27113ae65c2fcb69
Alicia Washington <aliciawashington874@gmail.com>
2019-10-24 22:49:39 -0400

    Superhero movie network request

--- Week1_Assignment_Flix/MoviesViewController.swift
+++ Week1_Assignment_Flix/MoviesViewController.swift
@@ -13,6 +13,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     
     @IBOutlet weak var tableView: UITableView!
+    
     var movies = [[String:Any]]()
     
 
