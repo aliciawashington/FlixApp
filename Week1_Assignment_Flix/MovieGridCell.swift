0628237bb433c1d750f080a3750e2a9467e6806e
Alicia Washington <aliciawashington874@gmail.com>
2019-10-24 23:42:18 -0400

    Collection View Setup

---
+++ Week1_Assignment_Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Week1_Assignment_Flix
+//
+//  Created by Alicia Washington on 10/24/19.
+//  Copyright © 2019 Alicia Washington. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
